f3e6430104dbde9c1062b0edee1fe20cc9af5589
Zeke Sikelianos <zeke@sikelianos.com>
2010-02-09 12:15:27 -0800

    Include @ in username link text

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,7 +1,7 @@
 module TweetsHelper
   
   def tweetify_text(text)
-    out = text.gsub(/@(\w*)/i) { link_to($1, "http://twitter.com/#{$1}") }
+    out = text.gsub(/@(\w*)/i) { link_to("@#{$1}", "http://twitter.com/#{$1}") }
     out = auto_link(out)
   end
   
